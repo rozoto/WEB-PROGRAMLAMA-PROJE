a883a0abfe5cc6ac7c12fc785d60e2024a4ecf3b
Nurgül Serin <79564224+rozoto@users.noreply.github.com>
2022-12-22 21:33:08 +0300

    Update HayvanlarController.cs
    eklemeler yapıldı

--- Controllers/HayvanlarController.cs
+++ Controllers/HayvanlarController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using yeni.Models;
 
 namespace yeni.Controllers
@@ -10,6 +10,36 @@ namespace yeni.Controllers
         {
             var degerler = c.Hayvanlars.ToList();
             return View(degerler);
+        }
+        public IActionResult İlanlar()
+        {
+            var degerler = c.Hayvanlars.ToList();
+            return View(degerler);
+        }
+        public IActionResult İletisim()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult İlanVer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult İlanVer(Hayvanlar h)
+        {
+            c.Hayvanlars.Add(h);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
         }
     }
 }
