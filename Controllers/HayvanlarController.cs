87d83fc89ebb37c11b86ecac9a670115a63c8934
Nurgül Serin <79564224+rozoto@users.noreply.github.com>
2022-12-23 21:08:25 +0300

    Update HayvanlarController.cs

--- Controllers/HayvanlarController.cs
+++ Controllers/HayvanlarController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using odevSon.Models;
 using yeni.Models;
 
 namespace yeni.Controllers
@@ -6,33 +8,69 @@ namespace yeni.Controllers
     public class HayvanlarController : Controller
     {
         Context c=new Context();
+        [AllowAnonymous]
+
         public IActionResult Index()
         {
             var degerler = c.Hayvanlars.ToList();
             return View(degerler);
         }
+        [AllowAnonymous]
+
         public IActionResult İlanlar()
         {
             var degerler = c.Hayvanlars.ToList();
             return View(degerler);
         }
+        [AllowAnonymous]
+
         public IActionResult İletisim()
         {
             return View();
         }
-
         [HttpGet]
         public IActionResult İlanVer()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult İlanVer(Hayvanlar h)
+        public IActionResult İlanVer(İlanOlustur h)
         {
-            c.Hayvanlars.Add(h);
+            Hayvanlar f=new Hayvanlar();
+            if (h.Hayvanİmage != null)
+            {
+                var extension = Path.GetExtension(h.Hayvanİmage.FileName);
+                var newimagename = Guid.NewGuid() + extension;
+                var location=Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/css/",newimagename);
+                var stream=new FileStream(location, FileMode.Create);
+                h.Hayvanİmage.CopyTo(stream);
+                f.Hayvanİmage = newimagename;
+            }
+            f.HayvanName = h.HayvanName;
+            f.HayvanCins=h.HayvanCins;
+            f.HayvanCinsiyet = h.HayvanCinsiyet;
+            f.HayvanIrk = h.HayvanIrk;
+            f.HayvanAgirlik = h.HayvanAgirlik;
+            c.Hayvanlars.Add(f);
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        [AllowAnonymous]
+
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+
+        public IActionResult HayvanBul()
+        {
+            return View();
+        }
+       
+    }
+}
+
         public IActionResult Kayit()
         {
             return View();
