834cc1cbd2de51301ab0848f4137bbd752ecf543
Nurgül Serin <79564224+rozoto@users.noreply.github.com>
2022-12-23 21:09:50 +0300

    Update Hayvanlar.cs

--- Models/Hayvanlar.cs
+++ Models/Hayvanlar.cs
@@ -11,5 +11,7 @@ namespace yeni.Models
         public int HayvanAgirlik { get; set; }
         public string  HayvanCinsiyet { get; set; }
         public int HayvanYas { get; set; }
+        public string HayvanCins { get; set; }
+        public string Hayvanİmage { get; set; }
     }
 }
