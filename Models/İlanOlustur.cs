cae410d63488dafd4ec1759e6df148fe2e6af9ce
Nurgül Serin <79564224+rozoto@users.noreply.github.com>
2022-12-30 23:26:23 +0300

    Update İlanOlustur.cs

--- Models/İlanOlustur.cs
+++ Models/İlanOlustur.cs
@@ -2,12 +2,12 @@ namespace odevvv.Models
 {
     public class İlanOlustur
     {
-        public string HayvanName { get; set; }
-        public string HayvanIrk { get; set; }
-        public int HayvanAgirlik { get; set; }
-        public string HayvanCinsiyet { get; set; }
-        public int HayvanYas { get; set; }
-        public string HayvanCins { get; set; }
-        public IFormFile Hayvanİmage { get; set; }
+        public string HayvanName  { get; set; }
+        public string HayvanIrk  { get; set; }
+        public int HayvanAgirlik  { get; set; }
+        public string HayvanCinsiyet  { get; set; }
+        public int HayvanYas  { get; set; }
+        public string HayvanCins  { get; set; }
+        public IFormFile Hayvanİmage  { get; set; }
     }
 }
