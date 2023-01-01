c64139ba4b0ee09c1b6350cdaaf7072d00644f72
Nurgül Serin <79564224+rozoto@users.noreply.github.com>
2023-01-01 21:13:02 +0300

    Add files via upload

---
+++ Controllers/İletisimController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using webOdev.Models;
+
+namespace webOdev.Controllers
+{
+    public class İletisimController : Controller
+    {
+        Context c = new Context();
+
+        [HttpGet]
+        public IActionResult İletisim()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult İletisim(İletisim p)
+        {
+            c.iletisims.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index","Hayvanlar");
+        }
+    }
+}
