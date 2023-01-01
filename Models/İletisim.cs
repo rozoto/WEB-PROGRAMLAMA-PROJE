d404f424bfeac74a09494b7b393eb622b9006c9f
Nurgül Serin <79564224+rozoto@users.noreply.github.com>
2023-01-01 21:11:51 +0300

    Add files via upload

---
+++ Models/İletisim.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webOdev.Models
+{
+    public class İletisim
+    {
+        [Key]
+        public int mesajID { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string country { get; set; }
+        public string subject { get; set; }
+    }
+}
